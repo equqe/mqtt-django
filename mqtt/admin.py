e596d189c554e46cea4594426cdbe3a9ab28c098
neumanncc <145790372+equqe@users.noreply.github.com>
2024-05-22 22:36:04 +0300

    Delete mqtt directory

--- mqtt/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .forms import MqttConfigForm
-from .models import *
-
-
-@admin.register(MqttConfig)
-class MqttConfigAdmin(admin.ModelAdmin):
-    form = MqttConfigForm
