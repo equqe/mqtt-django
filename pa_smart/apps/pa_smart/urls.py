035c4b2d4c44db8c1faf8b95ee6ddeac5e435062
neumanncc <145790372+equqe@users.noreply.github.com>
2024-05-22 22:11:28 +0300

    Delete pa_smart directory

--- pa_smart/apps/pa_smart/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from apps.users import views
-
-urlpatterns = [
-    path('accounts/', include("django.contrib.auth.urls")),
-    path('account/register/', views.RegisterView.as_view(), name='register'),
-    path('', views.index_view, name='index')
-]
